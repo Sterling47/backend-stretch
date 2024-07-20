6c8622ba83f85e037ac8d9ae21b5a5715468ef41
Adam Konber <konber3@gmail.com>
2024-07-20 11:24:03 -0600

    Added Router to hand Clicks, and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import '../src/App.css'
+import Header from './Components/Header/Header'
+import Cardcontainer from './Components/CardContainer/Cardcontainer'
+import Detailpage from './Components/DetailPage/Detailpage'
 
 const App = () => {
+  const location = useLocation()
+
   return (
     <div className='App'>
-      
+      {!location.pathname.startsWith('/detail') && <Header />}
+      <Routes>
+        <Route path="/" element={<Cardcontainer />} />
+        <Route path="/detail/:id" element={<Detailpage />} />
+      </Routes>
     </div>
   )
 }
+
 
 export default App
