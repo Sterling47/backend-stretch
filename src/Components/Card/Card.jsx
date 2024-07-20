4a8ae30e9048662d985115a228ae2a2bc648b8f9
Adam Konber <konber3@gmail.com>
2024-07-20 11:44:11 -0600

    Added a link to card to route to detail page, and styles

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../Card/Card.css'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <NavLink to='/detail/1' className='cardWrapper'>
+    <div className='card'></div>
+    </NavLink>
   )
 }
 
