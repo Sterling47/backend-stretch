ec96c37fb24b5ad576c0de252d0807af093352d1
David Swatzell <thatsstrikethree@gmail.com>
2024-07-20 14:39:53 -0500

    add h2 for header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ import '../Header/Header.css'
 const Header = () => {
   return (
     <header>
+      <h2>Food Ingredients Search</h2>
       <form action="search">
         <div>
           <input type="text" />
