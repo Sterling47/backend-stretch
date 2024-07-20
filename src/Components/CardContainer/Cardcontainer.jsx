e0efc40085e46bbac98a761747f398e9c68301b2
Adam Konber <konber3@gmail.com>
2024-07-20 11:45:24 -0600

    Added content to cardcontainer and a homebutton to the detailPage

--- src/Components/CardContainer/Cardcontainer.jsx
+++ src/Components/CardContainer/Cardcontainer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../CardContainer/Cardcontainer.css'
+import Card from '../Card/Card'
 
 const Cardcontainer = () => {
   return (
-    <div>Card-container</div>
+    <div className='cardContainer'>
+      <h2>Foods Containing common Allergens</h2>
+      <Card />
+    </div>
   )
 }
 
-export default Card-container
+export default Cardcontainer
--- src/Components/DetailPage/Detailpage.jsx
+++ src/Components/DetailPage/Detailpage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Detailpage = () => {
   return (
-    <div>Detailpage</div>
+    <div>Detailpage
+      <Link to='/' className='homeBttn'>Home</Link>
+    </div>
+   
   )
 }
 
