6b74aec5e1114bf2acbc1f9de074d5b28951672a
Zach Wolek <zach.wolek@gmail.com>
2024-07-23 18:59:36 -0500

    feat: run fetch request for all branded food categories, set as state, and pass as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import '../src/App.css'
 import Header from './Components/Header/Header'
@@ -8,9 +8,27 @@ import Detailpage from './Components/DetailPage/Detailpage'
 const App = () => {
   const location = useLocation()
 
+const [allBFCategories, setAllBFCategories] = useState([])
+
+const getBFCategories = () => {
+ return fetch("http://localhost:3001/api/food_categories")
+    .then((resp) => {
+      if(!resp.ok){
+        throw new Error('Response not okay!')
+      }
+      return resp.json()
+    })
+    .catch((error) => console.error('There has been an issue with request: ', error));
+}
+
+useEffect(() => {
+  getBFCategories()
+  .then(result => setAllBFCategories(result))
+}, [])
+
   return (
     <div className='App'>
-      {!location.pathname.startsWith('/detail') && <Header />}
+      {!location.pathname.startsWith('/detail') && <Header allBFCategories={allBFCategories}/>}
       <Routes>
         <Route path="/" element={<Cardcontainer />} />
         <Route path="/detail/:id" element={<Detailpage />} />
