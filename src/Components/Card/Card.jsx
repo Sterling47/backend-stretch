3c399f658a28ccb87bc76d53304a31bcbec392dc
Peter Kim <peterkim.pk1@gmail.com>
2024-07-24 17:49:06 -0400

    Styling changes on card container and added tags on detailed page

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({fdc_id,brand_name, description}) => {
   return (
     <Link to={`/detail/${fdc_id}`} className='cardWrapper'>
     <div className='card'>
-      <p>{fdc_id}</p>
+      <p className='fdc-id'> {fdc_id}</p>
       <p>{brand_name}</p>
       <p>{description}</p>
     </div>
--- src/Components/DetailPage/Detailpage.jsx
+++ src/Components/DetailPage/Detailpage.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom'
+import '../DetailPage/Detailpage.css'
 
 const Detailpage = ( { brandFoods, getFilteredFood} ) => { 
   const id = useParams();
   const singleFood = getFilteredFood(id.id)
-  const {fdc_id, brand_name, ingredients, serving_size} = singleFood
-  console.log('singleFood', singleFood)
-  //returned undefined 
+  const {fdc_id, brand_name, ingredients, serving_size, serving_size_unit} = singleFood
   
   return (
     <div>
       <h2>Detailpage</h2>
       <Link to='/' className='homeBttn'>Home</Link>
       <div>
-        <p>{fdc_id}</p>
-        <p>{brand_name}</p>
-        <p>{ingredients}</p>
-        <p>{serving_size}</p>
+        <p className='fdc-id'>fdc_id: {fdc_id}</p>
+        {brand_name ? <p>brand_name: {brand_name} </p> : <p>No matching brand name</p> }
+        <p>Ingredients: {ingredients}</p>
+        <p>Serving Size: {serving_size} {serving_size_unit}</p>
       </div>
     </div>
    
