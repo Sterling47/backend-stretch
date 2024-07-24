35b822d541adea983575d8f63c0f580f0e15bd71
Adam Konber <konber3@gmail.com>
2024-07-23 19:55:51 -0600

    feat: mapping over categoryFoods to create links for each food.
    Co-Authored-By: Peter Kim <peterkim.pk1@gmail.com>
    Co-Authored-By: Zach Wolek <zach.wolek@gmail.com>
    Co-Authored-By: Adam Konber <konber3@gmail.com>

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../Card/Card.css'
 
-const Card = () => {
+const Card = ({fdc_id,brand_name,description, serving_size, serving_size_unit}) => {
   return (
-    <Link to='/detail/1' className='cardWrapper'>
-    <div className='card'></div>
+    <Link to={`/detail/${fdc_id}`} className='cardWrapper'>
+    <div className='card'>
+      <h2>{brand_name}</h2>
+      <p>{description}</p>
+      <p>{serving_size}</p>
+      <p>{serving_size_unit}</p>
+    </div>
     </Link>
   )
 }
--- src/Components/CardContainer/Cardcontainer.jsx
+++ src/Components/CardContainer/Cardcontainer.jsx
@@ -3,15 +3,27 @@ import '../CardContainer/Cardcontainer.css'
 import Card from '../Card/Card'
 import {useEffect} from 'react'
 const Cardcontainer = ({categoryFoods}) => {
-  useEffect(() => {
-    getCategoryFood()
-  },[dropdown])
+  // useEffect(() => { 
+  //   getCategoryFood()
+  // },[dropdown])
+  const matchingFoods = categoryFoods.map(({id,fdc_id,brand_name,description,serving_size,serving_size_unit}) => {
+    return <Card
+    key={id} 
+    fdc_id={fdc_id}
+    brand_name={brand_name}
+    description={description}
+    serving_size={serving_size}
+    serving_size_unit={serving_size_unit}
+    />
+  })
   return (
     <div className='cardContainer'>
-      <h2>Foods Containing common Allergens</h2>
-      <Card />
+      {/* {!matchingFoods.brand_name ? <h2>No Foods to display </h2>:  <h2>Foods Containing{matchingFoods[0][branded_food_category]} } */}
+      {matchingFoods}
     </div>
   )
 }
 
 export default Cardcontainer
+
+//fdc_id,brand_name,description, serving_size, serving_size_unit
